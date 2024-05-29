9199ef2fa18c105805babf476202e12e26734d65
Allison Karp <akarp@mit.edu>
2024-05-28 20:31:21 -0400

    finished tutorial on youtube for basic weather app. deciding how i want to personally customize it now

--- weatherapp/lib/pages/weather_page.dart
+++ weatherapp/lib/pages/weather_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:weatherapp/services/weather_services.dart';
 import 'package:weatherapp/models/weather_model.dart';
+import 'package:lottie/lottie.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -35,6 +36,31 @@ class _WeatherPageState extends State<WeatherPage> {
   }
 
   // weather animations
+  // need to know which animation to show
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) return 'assets/sunny.json'; // default
+
+    switch (mainCondition.toLowerCase()){ // possible weather conditions from website
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/cloudy.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+      case 'thunderstorm':
+        return 'assets/stormy.json';
+      case 'clear':
+        return 'assets/sunny.json';
+      case 'snow':
+        return 'assets/snowy.json';
+      default:
+        return 'assets/sunny.json';
+    }
+  }
 
   // initial state
   @override
@@ -55,8 +81,15 @@ class _WeatherPageState extends State<WeatherPage> {
           children: [
             // city name
             Text(_weather?.cityName ?? "loading city..."),
+
+            // animations
+            Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+
             // temperature
-            Text('${_weather?.temperature.round()}F')
+            Text('${((_weather?.temperature ?? 0) * 9/5 + 32).round()}°F'),
+
+            // weather condition
+            Text(_weather?.mainCondition ?? "")
           ],
         ),
       ),
